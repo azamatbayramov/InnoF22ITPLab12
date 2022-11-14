a6b43274623b89e43cf677b9f3a2152ebe926076
Azamat Bayramov <bayramov.azamat04@gmail.com>
2022-11-14 13:55:00 +0300

    second task added

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,41 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        task2();
+    }
+
+    public static void task2() {
+        String inputFilename = getFilenameFromUserFor("input");
+        String outputFilename = getFilenameFromUserFor("output");
+        try (
+                FileInputStream fileInputStream = new FileInputStream(inputFilename);
+                FileOutputStream fileOutputStream = new FileOutputStream(outputFilename)
+        ) {
+            Scanner fileScanner = new Scanner(fileInputStream);
+            try {
+                if (!fileScanner.hasNextInt()) {
+                    throw new Exception("There is no number");
+                }
+                int a = fileScanner.nextInt();
+                if (!fileScanner.hasNextInt()) {
+                    throw new Exception("There is no number");
+                }
+                int b = fileScanner.nextInt();
+                if (b == 0) {
+                    throw new Exception("Division by zero");
+                }
+                fileOutputStream.write(Integer.toString(a / b).getBytes()[0]);
+            } catch (Exception e) {
+                reportError(e.getMessage());
+            }
+        } catch (FileNotFoundException e) {
+            reportError(e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void task1() {
         String inputFilename = getFilenameFromUserFor("input");
         String outputFilename = getFilenameFromUserFor("output");
         try (
@@ -31,5 +66,6 @@ public class Main {
 
     public static void reportError(String err) {
         System.out.println("ERROR:" + err);
+        System.exit(0);
     }
 }
